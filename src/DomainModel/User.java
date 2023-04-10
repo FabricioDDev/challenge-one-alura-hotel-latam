22741a675af8475dd82dffd55edd7bc60876515a
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-04-10 17:55:08 -0300

    UserClass-Attributes-Created

--- src/DomainModel/User.java
+++ src/DomainModel/User.java
@@ -1,5 +1,26 @@
 package DomainModel;
 
 public class User {
-
+//Id, UserName, Password.
+	private int Id;
+	private String UserName;
+	private String Password;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
 }
