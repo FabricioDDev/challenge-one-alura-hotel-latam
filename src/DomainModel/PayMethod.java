a30f278e76d6a5cd3cf4f157f7eaa7f06d9118f3
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-04-10 18:05:55 -0300

    PayMethod-Attributes-Created

--- src/DomainModel/PayMethod.java
+++ src/DomainModel/PayMethod.java
@@ -1,5 +1,19 @@
 package DomainModel;
 
 public class PayMethod {
-
+	private int Id;
+	private String MethodName;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getMethodName() {
+		return MethodName;
+	}
+	public void setMethodName(String methodName) {
+		MethodName = methodName;
+	}
+	
 }
