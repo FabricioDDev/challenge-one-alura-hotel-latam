2b12929976a54972990c50d0088b8f665bbec4c6
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2023-04-11 08:17:54 -0300

    DBFactory-Completed

--- src/DB/DBFactory.java
+++ src/DB/DBFactory.java
@@ -1,5 +1,13 @@
 package DB;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBFactory {
-
+	public Connection getConnection() throws SQLException  {
+			return DriverManager.getConnection(
+					"jdbc:mysql://localhost/AluraHotel-Esp-Base?useTimeZone=true&&serverTimeZone=UTC",
+					"root", "NOsequeponer10"
+					);
+	}
 }
